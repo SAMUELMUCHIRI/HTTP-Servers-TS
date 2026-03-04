e51e233278ad1d5a5657029672fd78cdd785e381
SAMUELMUCHIRI <eng.njaumuchiri@gmail.com>
2026-03-04 23:36:55 +0300

    DATABASE: Queries for performing CRUD operations on DB

---
+++ src/db/queries/chirps.ts
@@ -0,0 +1,22 @@
+import { db } from "../index.js";
+import { chirps, NewChirp } from "../schema.js";
+import { asc, eq } from "drizzle-orm";
+
+export async function createChirp(chirp: NewChirp) {
+  const [result] = await db
+    .insert(chirps)
+    .values(chirp)
+    .onConflictDoNothing()
+    .returning();
+  return result;
+}
+
+export async function allChirps() {
+  const results = await db.select().from(chirps).orderBy(asc(chirps.createdAt));
+  return results;
+}
+
+export async function getChirp(id: string) {
+  const [result] = await db.select().from(chirps).where(eq(chirps.id, id));
+  return result;
+}

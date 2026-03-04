e2ab785bf3bcf02b3eed3a3d95902c33d9559aee
SAMUELMUCHIRI <eng.njaumuchiri@gmail.com>
2026-03-04 23:39:00 +0300

    DATABASE: Loads enviroment variables from env  and types

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,34 @@
+process.loadEnvFile(".env");
+import type { MigrationConfig } from "drizzle-orm/migrator";
+
+type DBConfig = {
+  db: { url: string; migrationConfig: MigrationConfig };
+};
+
 type APIConfig = {
   fileserverHits: number;
 };
 
-export const config: APIConfig = {
+export const config: DBConfig & APIConfig = {
+  db: {
+    url: envOrThrow("DB_URL"),
+    migrationConfig: {
+      migrationsFolder: "./src/db/migrations",
+    },
+  },
   fileserverHits: 0,
 };
+
+const migrationConfig: MigrationConfig = {
+  migrationsFolder: "./src/db/migrations",
+};
+
+function envOrThrow(key: string): string {
+  const value = process.env[key];
+
+  if (value === undefined) {
+    throw new Error(`Missing environment variable: ${key}`);
+  }
+
+  return value;
+}

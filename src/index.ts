aee03045f006a12ebe99fee760479f596df9510a
SAMUELMUCHIRI <eng.njaumuchiri@gmail.com>
2026-03-04 23:41:47 +0300

    MAIN: added various endpoint for adding chrips, users . and fetching various data

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
 import express from "express";
-
+import postgres from "postgres";
+import { config } from "./config.js";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import { drizzle } from "drizzle-orm/postgres-js";
 import { middlewareLogResponses, middlewareMetricsInc } from "./middleware.js";
 import {
-  handlerReadiness,
+  healthHandler,
   hits,
   reset,
-  validateChirp,
+  createChirpHandler,
   errorHandler,
+  createUserHandler,
+  allChirpsHandler,
+  getChirpHandler,
 } from "./handlers.js";
 
 const app = express();
 const PORT = 8080;
-
+const migrationClient = postgres(config.db.url, { max: 1 });
+await migrate(drizzle(migrationClient), config.db.migrationConfig);
+//
 // Middleware
+//
 app.use(express.json());
 app.use(middlewareLogResponses);
 
@@ -22,8 +31,11 @@ app.use(middlewareLogResponses);
 app.use("/app", middlewareMetricsInc);
 app.use("/app", express.static("./src/app"));
 // API endpoints
-app.get("/api/healthz", handlerReadiness);
-app.post("/api/validate_chirp", validateChirp);
+app.get("/api/healthz", healthHandler);
+app.post("/api/chirps", createChirpHandler);
+app.post("/api/users", createUserHandler);
+app.get("/api/chirps", allChirpsHandler);
+app.get("/api/chirps/:chirpId", getChirpHandler);
 // Admin endpoints
 app.get("/admin/metrics", hits);
 app.post("/admin/reset", reset);

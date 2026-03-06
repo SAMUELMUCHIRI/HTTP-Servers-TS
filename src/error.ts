fa3011f53ab938c2616f76a9a931ecf3146f3528
SAMUELMUCHIRI <eng.njaumuchiri@gmail.com>
2026-03-06 22:55:17 +0300

    ERROR: added a custom error

--- src/error.ts
+++ src/error.ts
@@ -21,3 +21,9 @@ export class TokenError extends Error {
     super(message);
   }
 }
+
+export class UserNotFoundError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
+}
